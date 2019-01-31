70298cfed557ab50b6111301420e00fcb66e17e8
Ronique Young <youngronique09@gmail.com>
2019-01-30 23:40:07 -0500

    Used JavaFX to build SocialChat app GUI

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,23 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
+//import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+//import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			//BorderPane root = new BorderPane();
+			
+			Label label = new Label("Hello, User!");
+			label.setAlignment(Pos.CENTER);
+			Scene scene = new Scene(label,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("SocialChat");
 			primaryStage.setScene(scene);
