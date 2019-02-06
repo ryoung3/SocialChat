c1108384d7c342af6b522f3011da8eb4e2d2a064
Ronique Young <youngronique09@gmail.com>
2019-02-05 23:56:13 -0500

    Added list of current users on chat server

--- src/com/ryoung3/socialchat/chatserver/ChatServer.java
+++ src/com/ryoung3/socialchat/chatserver/ChatServer.java
@@ -1,5 +1,20 @@
 package com.ryoung3.socialchat.chatserver;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ryoung3.socialchat.users.User;
+
 public class ChatServer {
 
+	List<User> currentUsers = new ArrayList<>();
+	
+	
+	public void addUserToChat(User user){
+		currentUsers.add(user);
+	}
+	
+	public List<User> displayUsers(){
+		return currentUsers;
+	}
 }
