e46a88f21ec56dd567e8effbaf70a6b4c1050e42
Ronique Young <youngronique09@gmail.com>
2019-02-28 23:18:17 -0500

    Began developing code to connect to port

--- src/com/ryoung3/socialchat/chatserver/ChatServer.java
+++ src/com/ryoung3/socialchat/chatserver/ChatServer.java
@@ -1,5 +1,8 @@
 package com.ryoung3.socialchat.chatserver;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,8 +10,22 @@ import com.ryoung3.socialchat.users.User;
 
 public class ChatServer {
 
+	private int port;
 	List<User> currentUsers = new ArrayList<>();
 	
+	public ChatServer(int port) {
+		this.port = port;
+	}
+	
+	public void execute() {
+		try(ServerSocket serverSocket = new ServerSocket(port)){
+			System.out.println("Chat server is listening on port " + port);
+		}
+		catch(IOException ex) {
+			System.out.println("Error in the server: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
 	
 	public void addUserToChat(User user){
 		currentUsers.add(user);
