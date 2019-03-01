de938000fcc87478405c985b359d3f14b808d01d
Ronique Young <youngronique09@gmail.com>
2019-02-28 23:57:36 -0500

    Began developing chatClient class/code.

---
+++ src/com/ryoung3/socialchat/chatclient/ChatClient.java
@@ -0,0 +1,13 @@
+package com.ryoung3.socialchat.chatclient;
+
+public class ChatClient {
+	private String hostName;
+	private String userName;
+	private int port;
+	
+	public ChatClient(String hostName, int port) {
+		this.hostName = hostName;
+		this.port = port;
+	}
+	
+}
